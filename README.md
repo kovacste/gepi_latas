bf11a58ee1df698b55466d1df82024ad6aaab594
kovacste <kovacst.elod@gmail.com>
2021-11-15 18:15:04 +0100

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# gepi_latas
--- src/index.js
+++ src/index.js
@@ -1,36 +1,64 @@
 const imgElement = document.getElementById('imageSrc');
 const inputElement = document.getElementById('fileInput');
-const imageProps = document.getElementById('img-props');
+const inputImageProps = document.getElementById('input-img-props');
+const outputImageProps = document.getElementById('output-img-props');
+const debugSpace = document.getElementById('debug');
+const images = [];
+const video = document.getElementById('videoInput');
+const videoOutput = document.getElementById('canvasOutput');
+
+function drawDebugImages(images) {
+  images.forEach((image) => {
+      let canvas = document.createElement('canvas');
+      debugSpace.appendChild(canvas);
+      cv.imshow(canvas, image);  
+  });
+}
 
 inputElement.addEventListener('change', (e) => {
   imgElement.src = URL.createObjectURL(e.target.files[0]);
 }, false);
 
-imgElement.onload = function() {
+imgElement.onload = () => {
 
   const src = cv.imread(imgElement);
-  const edge = cannyEdge(src);
+  const edge = cannyEdge(src);  
   const resizedEdge = resizeImage(edge, { width: 500 });
-  const boundedEdge = drawBoundingRect(src);
+  const greyScale = toGreyScale(src);
+  const bluredGreyScale = blurImage(greyScale);
+  const binary = toTresholdedBinary(bluredGreyScale, { thresh: 140, maxval: 250 });
+  const contour = getContourImage(binary);
 
-  imageProps.innerText =  getImageProps(src);
+  images.push(src, edge, resizedEdge, greyScale, bluredGreyScale, binary, contour);
 
-  drawOnCanvas('canvas2', boundedEdge);
-  src.delete();
-  edge.delete();
-  resizedEdge.delete();
+  //const boundedEdge = drawBoundingRect(contour);
+  
+  drawContourOnImage(binary, src);
+
+  inputImageProps.innerText =  getImageProps(greyScale);
+  outputImageProps.innerText =  getImageProps(contour);
+
+  drawOnCanvas('canvas2', src);
+  drawDebugImages(images);
+
+  free();
 };
+
+window.onload = () =>  {
+  
+  startWebcamStream(video);
+  const videoProcessor = new VideoProcessor(video, videoOutput, 30);
+}
+
+
+function free() {
+  images.forEach((image) => {
+    image.delete();
+  });
+}
 
 function drawOnCanvas(canvas, src) {
   cv.imshow(canvas, src);
-}
-
-function cannyEdge(src, { threshold1 = 50, threshold2 = 100, apertureSize = 3, L2gradient = false } = {}) {
-  const dst = new cv.Mat();
-  const srcCopy = src.clone();
-  cv.cvtColor(srcCopy, srcCopy, cv.COLOR_RGB2GRAY, 0);
-  cv.Canny(srcCopy, dst, threshold1, threshold2, apertureSize, L2gradient);
-  return dst;
 }
 
 function getImageProps(src) {
@@ -42,48 +70,48 @@ function getImageProps(src) {
           'image type: ' + src.type() + '\n';
 }
 
-function resizeImage(src, { width, height }) {
-
-  let newWidth = width;
-  let newHeight = height;
-
-  if (!newWidth) {
-    const widthHeightRatio = src.cols / src.rows;
-    newWidth = height * widthHeightRatio;
-  }
-
-  if (!newHeight) {
-    const heightWidthRatio = src.rows / src.cols;
-    newHeight = width * heightWidthRatio;
-  }
-
-  const dst = new cv.Mat();
-  const dsize = new cv.Size(newWidth, newHeight);
-  cv.resize(src, dst, dsize, 0, 0, cv.INTER_AREA);
-  return dst;
+function startWebcamStream(videoElement) {
+  console.log('cucc');
+  navigator.mediaDevices.getUserMedia({video: true, audio: false}).then((stream) => {
+    videoElement.srcObject = stream;
+    videoElement.play();
+  }).catch((err) => {
+    console.log(err);
+  })
 }
 
-function drawBoundingRect(src) {
+const cfg = {
+  fps: 30,
+};
+function processVideo(video, canvas) {
+  let start = Date.now();
+  
+}
 
-  let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-  const srcCopy = src.clone();
-  cv.cvtColor(srcCopy, srcCopy, cv.COLOR_RGBA2GRAY, 0);
-  cv.threshold(srcCopy, srcCopy, 177, 200, cv.THRESH_BINARY);
-  let contours = new cv.MatVector();
-  let hierarchy = new cv.Mat();
-  cv.findContours(srcCopy, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-  let cnt = contours.get(0);
- 
-  let rect = cv.boundingRect(cnt);
-  let contoursColor = new cv.Scalar(255, 255, 0);
-  let rectangleColor = new cv.Scalar(255, 0, 0);
-  cv.drawContours(dst, contours, 0, contoursColor, 1, 8, hierarchy, 100);
-  let point1 = new cv.Point(rect.x, rect.y);
-  let point2 = new cv.Point(rect.x + rect.width, rect.y + rect.height);
-  cv.rectangle(dst, point1, point2, rectangleColor, 2, cv.LINE_AA, 0);
-  contours.delete();
-  hierarchy.delete();
-  cnt.delete();
 
-  return dst;
+function VideoProcessor(videoInput, canvasOutput, targetFps = 30) {
+
+  const context = canvasOutput.getContext('2d');
+  const fps = targetFps;
+  let src = new cv.Mat(height, width, cv.CV_8UC4);
+  let dst = new cv.Mat(height, width, cv.CV_8UC1);
+
+  function processVideo(){
+    let start = Date.now();
+    context.drawImage(videoInput, 0, 0, widht, height)
+    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+    cv.imshow(canvasOutput, dst);
+    let delay = 1000 / fps - (Date.now() - begin);
+    setTimeout(processVideo, delay);
+
+  }
+
+  function start() {
+    setTimeout(processVideo, 0);
+  }
+
+  return {
+    start
+  }
+
 }
